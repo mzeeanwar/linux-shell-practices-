85788071f91026dfa88498a39c60764a48e02a08
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-14 22:37:45 +0500

    Create forloop.sh to parse CSV into arrays
    Added a script to parse a CSV file into arrays and display the first column.

---
+++ forloop.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+csv_file="./arrays_table.csv"
+
+# parse table columns into 3 arrays
+column_0=($(cut -d "," -f 1 $csv_file))
+column_1=($(cut -d "," -f 2 $csv_file))
+column_2=($(cut -d "," -f 3 $csv_file))
+
+# print first array
+echo "Displaying the first column:"
+echo "${column_0[@]}"
+
+## Create a new array as the difference of columns 1 and 2
+# initialize array with header
+column_3=("column_3")
