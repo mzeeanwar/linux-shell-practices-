4ad5129ddd71004530ef0f3b6674282322a3b596
Zeeshan Anwar <ishanrajpoot78@gmail.com>
2026-05-14 22:38:15 +0500

    Add functionality to compute and save column differences
    Calculate the difference between two columns and save results.

--- forloop.sh
+++ forloop.sh
@@ -14,3 +14,20 @@ echo "${column_0[@]}"
 ## Create a new array as the difference of columns 1 and 2
 # initialize array with header
 column_3=("column_3")
+# get the number of lines in each column
+nlines=$(cat $csv_file | wc -l)
+echo "There are $nlines lines in the file"
+# populate the array
+for ((i=1; i<$nlines; i++)); do
+  column_3[$i]=$((column_2[$i] - column_1[$i]))
+done
+echo "${column_3[@]}"
+
+## Combine the new array with the csv file
+# first write the new array to file
+# initialize the file with a header
+echo "${column_3[0]}" > column_3.txt
+for ((i=1; i<nlines; i++)); do
+  echo "${column_3[$i]}" >> column_3.txt
+done
+paste -d "," $csv_file column_3.txt > report.csv
